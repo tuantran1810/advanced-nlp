be199f0f3ac0127815c4f9f899181a097f4e5040
tuantran <tuantran1810@github.com>
2020-11-30 22:33:04 +0700

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from execute import CharInference
 
 def main():
-    char_inference = CharInference("./name_data_char_sequences.txt")
+    char_inference = CharInference("./data/name_data_char_sequences.txt")
     char_inference.train(5)
 
 if __name__ == "__main__":
